18744b768e14b1c15a8c6326aa913b420d9bb30f
Masaaki <147675004+yndzm@users.noreply.github.com>
2023-11-23 16:04:49 +0800

    Delete config/x86/openwrt-22.03/config.sh

--- config/x86/openwrt-22.03/config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone --depth 1 -b x86_64 --single-branch $1 /workdir/config
-cd /workdir/config
-mkdir -p /workdir/openwrt/files
-mv etc /workdir/openwrt/files/
-mv root /workdir/openwrt/files/
